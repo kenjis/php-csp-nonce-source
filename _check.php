4942778d9cd9e779098fd03916137c6dca8d6792
kenjis <kenji.uui@gmail.com>
2014-10-25 23:23:00 +0900

    Add script to check CSP nonce-source support

---
+++ _check.php
@@ -0,0 +1,28 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+header("Content-Security-Policy:script-src 'nonce-sXXD/nluT6AqhuVwL0IJqA=='");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Sample of CSP nonce-source</title>
+</head>
+<body>
+
+<script type="text/javascript" nonce="sXXD/nluT6AqhuVwL0IJqA==">
+    alert('This works!');
+</script>
+
+<script type="text/javascript">
+    alert('This does not work!');
+</script>
+
+<?php
+$classifier = new \Woothee\Classifier;
+$r = $classifier->parse($_SERVER['HTTP_USER_AGENT']);
+var_dump($r);
+?>
+
+</body>
+</html>
