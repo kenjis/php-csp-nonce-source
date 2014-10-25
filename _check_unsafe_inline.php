05f3fcb9232c0b8bdf3bccd8ab5e3fab925ec665
kenjis <kenji.uui@gmail.com>
2014-10-25 23:40:23 +0900

    Add sample with both unsafe-inline and nonce

---
+++ _check_unsafe_inline.php
@@ -0,0 +1,21 @@
+<?php
+header("Content-Security-Policy: script-src 'unsafe-inline' 'nonce-sXXD/nluT6AqhuVwL0IJqA=='");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Sample of CSP nonce-source</title>
+</head>
+<body>
+
+<script type="text/javascript" nonce="sXXD/nluT6AqhuVwL0IJqA==">
+    alert('This works!');
+</script>
+
+<script type="text/javascript">
+    alert('This does not work!');
+</script>
+
+</body>
+</html>
