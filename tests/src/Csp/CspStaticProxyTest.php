15c4147c6fbaadc565a0eddffbe0e4008a12eb71
kenjis <kenji.uui@gmail.com>
2014-10-28 18:33:51 +0900

    Add tests

--- tests/src/Csp/CspStaticProxyTest.php
+++ tests/src/Csp/CspStaticProxyTest.php
@@ -76,4 +76,34 @@ class CspStaticProxyTest extends TestCase
             ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/600.1.25 (KHTML, like Gecko) Version/8.0 Safari/600.1.25'],
         ];
     }
+
+    public function test_setReportOnly()
+    {
+        $_SERVER['HTTP_USER_AGENT'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:33.0) Gecko/20100101 Firefox/33.0';
+
+        test::func(__NAMESPACE__, 'openssl_random_pseudo_bytes', '1234567890123456');
+        $func = test::func(__NAMESPACE__, 'header', '');
+
+        CspStaticProxy::setReportOnly();
+        CspStaticProxy::sendHeader();
+
+        $func->verifyInvoked(
+            ["Content-Security-Policy-Report-Only: script-src 'nonce-MTIzNDU2Nzg5MDEyMzQ1Ng=='; report-uri /csp-report.php"]
+        );
+    }
+
+    public function test_addPolicy()
+    {
+        $_SERVER['HTTP_USER_AGENT'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:33.0) Gecko/20100101 Firefox/33.0';
+
+        test::func(__NAMESPACE__, 'openssl_random_pseudo_bytes', '1234567890123456');
+        $func = test::func(__NAMESPACE__, 'header', '');
+
+        CspStaticProxy::addPolicy('connect-src', 'example.com');
+        CspStaticProxy::sendHeader();
+
+        $func->verifyInvoked(
+            ["Content-Security-Policy: script-src 'nonce-MTIzNDU2Nzg5MDEyMzQ1Ng=='; report-uri /csp-report.php; connect-src example.com"]
+        );
+    }
 }
