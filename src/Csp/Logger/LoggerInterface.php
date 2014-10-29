d9b3879eb95ad739cddb8e85e8b4641b1af7b23f
kenjis <kenji.uui@gmail.com>
2014-10-29 21:19:37 +0900

    Add license comment
    [ci skip]

--- src/Csp/Logger/LoggerInterface.php
+++ src/Csp/Logger/LoggerInterface.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * CSP
+ *
+ * @author     Kenji Suzuki <https://github.com/kenjis>
+ * @license    MIT License
+ * @copyright  2014 Kenji Suzuki
+ * @link       https://github.com/kenjis/php-csp-nonce-source
+ */
 
 namespace Kenjis\Csp\Logger;
 
